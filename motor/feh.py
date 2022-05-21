6eba3a4c1fbbb0c26d82eb39e6d10ab3579db77b
0111kk2 <kokeshi.120.st@gmail.com>
2022-05-22 01:18:39 +0900

    feh（モーター動作用）を作成

---
+++ motor/feh.py
@@ -0,0 +1,34 @@
+
+import RPi.GPIO as GPIO
+import time
+
+SERVO_PIN_R = 16
+SERVO_PIN_L = 18
+
+pi=GPIO.pi()
+
+while True:
+    #　速く前進
+    pi.set_servo_pulsewidth( SERVO_PIN_R, 1000 )
+    pi.set_servo_pulsewidth( SERVO_PIN_L, 2000 )
+    time.sleep( 2 )
+    #　遅く前進
+    pi.set_servo_pulsewidth( SERVO_PIN_R, 1300 )
+    pi.set_servo_pulsewidth( SERVO_PIN_L, 1700 )
+    time.sleep( 2 )
+    #　速く後退
+    pi.set_servo_pulsewidth( SERVO_PIN_R, 2000 )
+    pi.set_servo_pulsewidth( SERVO_PIN_L, 1000 )
+    time.sleep( 2 )
+    #　遅く後退
+    pi.set_servo_pulsewidth( SERVO_PIN_R, 1700 )
+    pi.set_servo_pulsewidth( SERVO_PIN_L, 1300 )
+    time.sleep( 2 )
+    #　右超信地
+    pi.set_servo_pulsewidth( SERVO_PIN_R, 1700 )
+    pi.set_servo_pulsewidth( SERVO_PIN_L, 1700 )
+    time.sleep( 2 )
+    #　左超信地
+    pi.set_servo_pulsewidth( SERVO_PIN_R, 1300 )
+    pi.set_servo_pulsewidth( SERVO_PIN_L, 1300 )
+    time.sleep( 2 )
