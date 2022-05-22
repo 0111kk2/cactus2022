265fcbc89539ed0ee7d69bf769fd80c2ed637909
0111kk2 <kokeshi.120.st@gmail.com>
2022-05-22 13:37:13 +0900

    Update BME280.py

--- sensor/BME280.py
+++ sensor/BME280.py
@@ -12,20 +12,21 @@ print('湿度 {}%'.format(bme280.humidity))  # 湿度を取得して表示
 print('気圧 {}hPa'.format(bme280.pressure))  # 気圧を取得して表示
 
 
-#if __name__ == "__main__": #ターミナルから実行した場合
-#    time.sleep(0.1)  
-#    now_time = datetime.datetime.now()
-#    filename = 'BME280' + now_time.strftime('%Y%m%d_%H%M%S') + '.csv'
-#    # ファイル，1行目(カラム)の作成
-#    with open(filename, 'a') as f:
-#        writer = csv.writer(f)
-#        writer.writerow(['temperature', 'humidity', 'pressure'])
-#    while True:
-#        bme280 = cgsensor.BME280(i2c_addr=0x76) 
-#        print("temperature:{}, humidity:{}, pressure:{}".format(bme280.temperature,bme280.humidity, bme280.pressure))
-#        print("")
-#        print("\n")
-#        time.sleep(0.1)
-#        with open(filename, 'a', newline="") as f:
-#            writer = csv.writer(f)
-#            writer.writerow([bme280.temperature,bme280.humidity, bme280.pressure])
+if __name__ == "__main__": #ターミナルから実行した場合
+    time.sleep(0.1)  
+    now_time = datetime.datetime.now()
+    filename = 'BME280' + now_time.strftime('%Y%m%d_%H%M%S') + '.csv'
+    # ファイル，1行目(カラム)の作成
+    with open(filename, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(['temperature', 'humidity', 'pressure'])
+    while True:
+        bme280 = cgsensor.BME280(i2c_addr=0x76)
+        bme280.forced() 
+        print("temperature:{}, humidity:{}, pressure:{}".format(bme280.temperature,bme280.humidity, bme280.pressure))
+        print("")
+        print("\n")
+        time.sleep(0.1)
+        with open(filename, 'a', newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([bme280.temperature,bme280.humidity, bme280.pressure])
