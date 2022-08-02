e8580c7eb4abbba27f3af9c6f417e38ee89fe06a
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-08-02 14:39:50 +0900

    change port

--- sensor/communication/xbee.py
+++ sensor/communication/xbee.py
@@ -8,7 +8,7 @@ def str_trans(string):
     """
     string = str(string)
     ser = serial. Serial(
-        port="/dev/serial0",
+        port="/dev/ttyS0",
         baudrate=9600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
