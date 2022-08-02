063c8b396d75a358400451215fe334628beeb300
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-08-02 16:06:57 +0900

    while add

--- gps_xbee.py
+++ gps_xbee.py
@@ -4,16 +4,21 @@ from sensor.GPS import gps
 
 if __name__ == "__main__":
     gps.open_gps()
-    utc, lat, lon, sHeight, gHeight = gps.read_gps()
-    if utc == -1.0:
-        if lat == -1.0:
-            print("Reading gps Error")
-            xbee.str_trans("Reading gps Error")
-            # pass
-        else:
-            # pass
-            print("Status V")
-            xbee.str_trans("Status V")
-    else:
-        # pass
-        print(utc, lat, lon, sHeight, gHeight)
+    try:
+
+        while 1:
+            utc, lat, lon, sHeight, gHeight = gps.read_gps()
+            if utc == -1.0:
+                if lat == -1.0:
+                    print("Reading gps Error")
+                    xbee.str_trans("Reading gps Error")
+                    # pass
+                else:
+                    # pass
+                    print("Status V")
+                    xbee.str_trans("Status V")
+            else:
+                # pass
+                print(utc, lat, lon, sHeight, gHeight)
+    except:
+        gps.close_gps()
