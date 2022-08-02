5cb16afe3a624913bad935f2d70d0478c1383c73
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-08-02 14:20:01 +0900

    xbee確認するためのファイル作りました

---
+++ sensor/communication/xbee.py
@@ -0,0 +1,34 @@
+import serial
+# from time import time
+import time
+def str_trans(string):
+    """
+    文字列を送る関数
+    データを送る際は必ずstr型に直して引数に入れる。
+    """
+    string = str(string)
+    ser = serial. Serial(
+        port="/dev/ttyAMA0",
+        baudrate=57600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=5
+    )
+    string = string + '\n'
+    moji = string.encode()
+    commands = [moji]
+    for cmd in commands:
+        ser.write(cmd)
+    ser.flush()
+    ser.close()
+
+if __name__ =="__main__":
+    str1 = "aaaa"
+    str_trans(str1)
+    print(str1)
+    time.sleep(1)
+    str2 = 123
+    str2 = str(str2)
+    str_trans(str2)
+    print(str2)
