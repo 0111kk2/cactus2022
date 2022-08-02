e707a419c250a85c3b971128ea2e9c67c36e4fb5
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-08-02 14:37:21 +0900

    port ボートレート変更

--- sensor/communication/xbee.py
+++ sensor/communication/xbee.py
@@ -8,8 +8,8 @@ def str_trans(string):
     """
     string = str(string)
     ser = serial. Serial(
-        port="/dev/ttyAMA0",
-        baudrate=57600,
+        port="/dev/serial0",
+        baudrate=9600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
