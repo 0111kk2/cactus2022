21062edb575cfdd55a0885fbd876f395eb9e9572
gitub <82073759+oshima-yoppi@users.noreply.github.com>
2022-08-02 16:03:56 +0900

    xbee とGPSのテスト

---
+++ gps_xbee.py
@@ -0,0 +1,19 @@
+from unicodedata import name
+from sensor.communication import xbee
+from sensor.GPS import gps
+
+if __name__ == "__main__":
+    gps.open_gps()
+    utc, lat, lon, sHeight, gHeight = gps.read_gps()
+    if utc == -1.0:
+        if lat == -1.0:
+            print("Reading gps Error")
+            xbee.str_trans("Reading gps Error")
+            # pass
+        else:
+            # pass
+            print("Status V")
+            xbee.str_trans("Status V")
+    else:
+        # pass
+        print(utc, lat, lon, sHeight, gHeight)
